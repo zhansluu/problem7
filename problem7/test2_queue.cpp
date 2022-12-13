3264d0d2d850be8aa35170b0a30694af52ca701b
Nikita Yu Lovyagin <lovyagin@mail.com>
2022-12-13 14:16:19 +0300

    Review

---
+++ problem7/test2_queue.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+
+#include "queue.h"
+
+using namespace std;
+
+template <class T> class numerate
+{
+public:
+    numerate(T start = 0, T _shift = 1): seed(start), shift(_shift) {};
+    T operator () () {T old = seed; seed += shift; return old; }
+private:
+    T seed, shift;    
+};
+
+template <class T, class C> T sum (const C& c)
+{
+    T res = 0;
+    for (typename C::const_iterator i = c.begin(); i != c.end(); ++i)
+       res += *i;
+    return res;
+}
+
+int main()
+{
+    Queue s1, s2;
+    numerate f(100);
+
+    s1.Push(1);
+    s1.Push(2);
+    s1.Push(3);
+    
+    s2.Push(5);
+    s2 = s1;
+    s2.Push(4);
+    
+    cout << s1 << endl;          // 1->2->3
+    cout << s1.Size() << endl;
+    
+    cout << s2 << endl;          // 1->2->3->4
+    cout << s2.Size() << endl;
+    
+    swap(s1, s2);
+    
+    cout << s1 << endl;          // 1->2->3->4
+    cout << s1.Size() << endl;
+    
+    cout << s2 << endl;          // 1->2->3
+    cout << s2.Size() << endl;
+    
+    generate(s1.begin(), s1.end(), f);
+
+    cout << s1 << endl;          // 100->101->102->103
+    cout << s1.Size() << endl;
+    
+    cout << sum<int,single_linked_list>(s2) << endl;     // 6
+
+    return 0;
+}
